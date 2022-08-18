86ce3be8ef9cb1e50a007b3c3d10ea31b03373fb
eunjaeyoon <yoonej111@gmail.com>
2022-08-18 10:59:35 +0900

    feat: 메시지 추가

--- client/src/pages/home/Section3.jsx
+++ client/src/pages/home/Section3.jsx
@@ -1,10 +1,12 @@
-import { Image, Row } from 'antd';
+import { Result, Image, Row } from 'antd';
+import { SmileOutlined } from '@ant-design/icons';
 import React from 'react';
 import mainImg from '../../../src/asset/imgs/Recent_NFTs.png';
 
 function Section3() {
   return (
     <Row gutter={[8, 8]} justify="center" align="middle" wrap={true}>
+      <Result icon={<SmileOutlined />} title="Great, we have done all the operations!" />
       <Image src={mainImg} alt="main image" preview={false} />
     </Row>
   );
