36ac5ada799c42c2fa5507cc3ea6cb9e01e963dd
dalja6309@gmail.com <haoh@cern.ch>
2022-08-19 16:40:31 +0900

    add mypage element

--- client/src/router/Router.jsx
+++ client/src/router/Router.jsx
@@ -5,6 +5,8 @@ import Section2 from '../pages/home/Section2';
 import Section3 from '../pages/home/Section3';
 import NotAuthorized from '../pages/NotAuthorized';
 import MyPage from '../pages/MyPage/MyPage';
+import MP_sec1 from '../pages/MyPage/MP_sec1';
+import MP_sec2 from '../pages/MyPage/MP_sec2';
 import Posts from '../pages/Posts/Posts';
 import Market from '../pages/Market/Market';
 import Create from '../pages/Create/Create';
@@ -16,7 +18,7 @@ export default function Router() {
       <Route path="/" element={[<Section1 />, <Section2 />, <Section3 />]} />
       <Route path="/posts" element={[<Posts />, <NotAuthorized />]} />
       <Route path="/posts/:content_id" element={<PostDetail />} />
-      <Route path="/mypage" element={[<MyPage />, <NotAuthorized />]} />
+      <Route path="/mypage" element={[<MyPage />, <MP_sec1 />, <MP_sec2 />]} />
       <Route path="/create" element={[<Create />, <NotAuthorized />]} />
       <Route path="/market" element={[<Market />, <NotAuthorized />]} />
     </Routes>
