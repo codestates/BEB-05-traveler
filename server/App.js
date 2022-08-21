e7b39c383021cff98702ab14a5da854159e3def2
PARKINKYUN <sefthia@gmail.com>
2022-08-22 01:15:18 +0900

    controllers & models 02

--- server/App.js
+++ server/App.js
@@ -1,30 +1,35 @@
 const express = require("express");
 const app = express();
-
-const mongoose = require('mongoose');
+const fs = require('fs');
+const https = require('https');
+const cors = require('cors');
 
 const dotenv = require('dotenv');
 dotenv.config();
-const url = process.env.MongoDB_url;
+
+const mongoConnect = require('./models');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+mongoConnect();
 
 const routes = require("./routes");
 
 app.use("/", routes);
 
-mongoose.connect(url, (err) => {
-    if(err) {
-        console.log(err);
-    }
-    else { 
-        console.log('connected to database successfully')
-    }
-});
+const PORT = process.env.HTTPS_PORT || 4000;
 
-const PORT = process.env.PORT || 4000;
+let server;
+if(fs.existsSync('./key.pem') && fs.existsSync('./cert.pem')){
+    const privateKey = fs.readFileSync(__dirname + '/key.pem', 'utf8');
+    const certificate = fs.readFileSync(__dirname + '/cert.pem', 'utf8');
+    const credentials = { key: privateKey, cert: certificate };
 
-module.exports = app.listen(PORT, () => {
-    console.log("start")
-});
+    server = https.createServer(credentials, app);
+    server.listen(PORT, ()=> console.log('Server is working!'));
+} else {
+    server = app.listen(PORT)
+}
+
+module.exports = server;
--- server/controllers/main.controller.js
+++ server/controllers/main.controller.js
@@ -1,9 +1,12 @@
-const model = require('../models');
+const postmodel = require('../models/post');
+const nftmodel = require('../models/nft')
 
 module.exports = {
-    findAll: (req, res) => {
-        console.log('모든 게시물과 NFT를 가져옵니다.')
-        return res.status(200).send('모든 게시물과 NFT를 가져옵니다.')
+    // 모든 게시물과 NFT 정보 조회
+    findAll: async (req, res) => {
+        const postInfo = await postmodel.getAllPosts();
+        const nftInfo = await nftmodel.getAllNfts();
 
+        return res.status(200).send({data: {postInfo, nftInfo}, message: "Completed search"})
     }
 }
