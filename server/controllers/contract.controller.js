34f48688cc7997269f320abcda4c2c41125c89ad
PARKINKYUN <sefthia@gmail.com>
2022-08-22 16:42:13 +0900

    token 거래

--- server/controllers/contract.controller.js
+++ server/controllers/contract.controller.js
@@ -8,20 +8,21 @@ const nftmodel = require("../models/nft");
 const usermodel = require("../models/user");
 const Web3 = require("web3");
 
-const web3 = new Web3(env.process.RPCURL);
+const web3 = new Web3(process.env.RPCURL);
 // 스마트컨트랙트 객체 생성. ERC20 / ERC721.
 const contract20 = new web3.eth.Contract(abi20, address20);
 const contract721 = new web3.eth.Contract(abi721, address721);
 
 const getTransactions = cron.schedule('* * * * * *', async function () {
     // 블록체인에 기록된 nft의 수와 DB에 저장된 nft의 수를 가져온다.
+    console.log("1초마다 블록체인네트워크를 탐색합니다.")
     const totalSupply = await contract721.methods.totalSupply().call();
     const nftCount = await nftmodel.getAllNfts().sort({ token_id: -1 }).limit(1);
 
     // 새로운 nft만 추가해서 DB에 적재한다.
     for ( let i = nftCount+1 ; i <= totalSupply ; i++ ){
-        const user_address = await contract721.methods.ownerOf(i);
-        const token_uri = await contract721.methods.tokenURI(i);
+        const user_address = await contract721.methods.ownerOf(i).call();
+        const token_uri = await contract721.methods.tokenURI(i).call();
         const userInfo = await usermodel.getUserInfoByAddress(user_address);
         const newNft = {
             user_id: userInfo.user_id,
@@ -31,6 +32,8 @@ const getTransactions = cron.schedule('* * * * * *', async function () {
         }
         await nftmodel.saveNft(newNft);
     }
+},{
+    scheduled: false
 })
 
 module.exports = getTransactions;
