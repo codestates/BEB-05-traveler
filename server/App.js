6893c7b9b3add5ea00541367e62e462b1d965d36
dalja6309@gmail.com <haoh@cern.ch>
2022-08-23 22:22:24 +0900

    App.js, borad.controller.js, post.js 수정

--- server/App.js
+++ server/App.js
@@ -7,8 +7,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+app.use(express.urlencoded({limit:"50mb", extended: false}));
+app.use(express.json({limit : "50mb"}));
 app.use(cors());
 
 const mongoConnect = require('./models');
