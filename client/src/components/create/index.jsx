d10bdd76101b905da244158e7aa3105bffb5d383
PARKINKYUN <sefthia@gmail.com>
2022-08-24 17:39:13 +0900

    8/24 server update

--- client/src/components/create/index.jsx
+++
@@ -1,7 +0,0 @@
-import InputDesctiption from './InputDesctiption';
-import InputName from './InputName';
-import UploadImage from './UploadImage';
-import ButtonMint from './ButtonMint';
-import CreatePageTitle from './CreatePageTitle';
-
-export { InputDesctiption, InputName, UploadImage, ButtonMint, CreatePageTitle };
