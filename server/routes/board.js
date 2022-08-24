45201ccd7d0680bffbcbfb2a07e916da25619f4b
dalja6309@gmail.com <haoh@cern.ch>
2022-08-24 18:01:03 +0900

    board.js 수정

--- server/routes/board.js
+++ server/routes/board.js
@@ -7,6 +7,6 @@ router.post("/newpost", controller.newpost);
 router.post("/post_update", controller.post_update);
 router.post("/post_delete", controller.post_delete);
 router.get("/postbyid", controller.postbyid); // id로 게시물 조회하는 부분 추가
-router.get("/postbypostid", controller.postbypostid);
+router.get("/post/:post_id:", controller.postbypostid);
 
 module.exports = router;
