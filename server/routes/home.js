5e98f455ed5082d393dd63a6861b03f7a8ce58e1
Hayoung Oh <104472372+Hayoung5@users.noreply.github.com>
2022-08-24 17:52:32 +0900

    Revert "Dev"

--- server/routes/home.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/main.controller");
-
-router.get("/", controller.findAll);
-
-module.exports = router;
--- server/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/user.controller");
-
-router.post("/login", controller.login);
-router.post("/join", controller.join);
-router.get("/info", controller.info);
-
-module.exports = router;
